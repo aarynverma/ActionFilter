4222ace91cf879d916f7da08ec25eb3a168791a8
aarynverma <18100BTCSE02667@svvvindore.onmicrosoft.com>
2022-08-24 11:39:44 +0530

    Add project files.

---
+++ MysampleActionFilter.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ActionFilter
+{
+    public class MysampleActionFilter : IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            Console.WriteLine("OnActionExecuted");
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            Console.WriteLine("OnActionExecuting");
+        }
+    }
+}
